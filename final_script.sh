095fcc79895d08f3ce6151558bc5c9251461eb45
vivekbhat <vivekbhat@live.com>
2017-11-05 16:45:44 +0000

    final script

---
+++ final_script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+node bit.js
+
+var="$(cat foo.txt)"
+echo $var
+
+sed -i "s/.*The value has changed.*/        this.emit(':tell', 'The value has changed by $var in the last 24 hours');/" index.js
+
+sentiment="$(cat result.txt)" 
+sed -i "s/.*The current.*/        this.emit(':tell', '$sentiment');/" index.js
+
+
+mkdir -p src
+cp index.js src/index.js
+rsync -r node_modules src
+cd src
+
+zip -r ../src.zip *
+
+cd ..
+
+aws s3 cp src.zip  s3://hackncvivek/
+
+
