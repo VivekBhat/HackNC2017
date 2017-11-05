1487790f48cbb5b2ae8948848116c2c63f0bc5b1
Vivek Bhat <vivekbhat@live.com>
2017-11-04 21:20:33 -0400

    coinbase api

--- NodeJs/main.js
+++ NodeJs/main.js
@@ -3,21 +3,24 @@ var coinbase = require('coinbase');
 var coinbase_access_key = process.env.COINBASE_API_KEY;
 var coinbase_secret_key = process.env.COINBASE_API_SECRET;
 
-console.log(process.env);
-
-console.log(coinbase_access_key);
 var client   = new coinbase.Client({'apiKey': coinbase_access_key, 'apiSecret': coinbase_secret_key});
 
-// var Client = require('coinbase').Client;
-// var client = new Client({'accessToken': accessToken, 'refreshToken': refreshToken});
+var logger = require('logops');
 
+logger.setLevel('DEBUG');
 
-// client.getAccounts({}, function(err, accounts) {
-//   accounts.forEach(function(acct) {
-//     console.log('my bal: ' + acct.balance.amount + ' for ' + acct.name);
-//   });
+logger.debug('This is an example');
+
+currencyCode = 'USD'  // can also use EUR, CAD, etc.
+
+// client.getBuyPrice({'currencyPair': 'BTC-USD'}, function(err, obj) {
+// console.log('total amount: ' + obj.pagination);
 // });
-
-client.getBuyPrice({'currencyPair': 'BTC-USD'}, function(err, obj) {
-  console.log('total amount: ' + obj.data.amount);
+// 
+client.getSpotPrice({'currency': currencyCode,'date': '2016-10-02'}, function(err, price) {
+  console.log('Current bitcoin price in ' + currencyCode + ': ' +  price.data.amount);
 });
+
+// client.getTime(function(err, time) {
+//   console.log(time.data.iso);
+// });
